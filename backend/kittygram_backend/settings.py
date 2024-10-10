455b4bb33763ba9e49fd83f6073ce7729d58d512
ivxmirov <ivxmirov@gmail.com>
2024-10-10 22:19:32 +0400

    ready for review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = (os.getenv('DEBUG') or 'false').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '89.169.166.171', 'ivxforstudy.ru']
+ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS') or []).split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
